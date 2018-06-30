a3ca8227a2b39c190b036f9d3ad841d15e747bdd
Maurício Szabo <mauricio.szabo@gmail.com>
2018-06-30 00:06:58 -0300

    Adding HSQLDB for tests

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [honeysql "0.9.2"]]
-  :profiles {:dev {:dependencies [[midje "1.9.1"]]}})
+  :profiles {:dev {:dependencies [[midje "1.9.1"]
+                                  [org.hsqldb/hsqldb "2.4.0"]]}})
