e250ca57c38ba7e5b1eb1f2ff58583a846959956
Maurício Szabo <mauricio.szabo@gmail.com>
2018-11-18 23:33:02 -0200

    Completing hierarchical data

--- src/bee_record/walker.cljc
+++ src/bee_record/walker.cljc
@@ -1,6 +1,47 @@
 (ns bee-record.walker
-  (:require [clojure.walk :as walk]
-            [clojure.set :as set]))
+  (:require [clojure.set :as set]))
+
+(defn- table-for [entity]
+  (let [from (:from entity)]
+    (cond
+      (keyword? from) from
+      (vector? from) (let [from-part (first from)]
+                       (if (vector? from-part) (second from-part) from-part)))))
+
+(defn- complete-hierarchy [orig-hierarchy all-keys]
+  (loop [hierarchy orig-hierarchy
+         [key & ks] all-keys]
+    (if key
+      (let [map (key hierarchy)
+            my-keys (keys map)
+            to-complete (set/difference all-keys (keys map) #{key})
+            completions (for [my-key my-keys
+                              new-key to-complete
+                              :let [val (get-in orig-hierarchy [my-key new-key])]
+                              :when val]
+                          [new-key my-key])
+            new-hierarchy (update hierarchy key merge (into {} completions))]
+
+        (cond
+          (-> completions count (= (count to-complete)))
+          (recur new-hierarchy ks)
+          ;
+          (-> completions count zero?)
+          (throw (ex-info "Join graph does not compute" {:entity key
+                                                         :missing-associations to-complete}))
+
+          :else
+          (recur new-hierarchy (cons key ks))))
+      hierarchy)))
+
+(defn- create-hierarchy [mapping]
+  (let [entity-for (fn [e-name] (-> mapping (get-in [:entities e-name]) table-for))
+        aggregate (fn [joins [e1 e2] conditions]
+                    (-> joins
+                        (assoc-in [e1 e2] [(entity-for e2) conditions])
+                        (assoc-in [e2 e1] [(entity-for e1) conditions])))
+        orig-hierarchy (reduce-kv aggregate {} (:joins mapping))]
+    (complete-hierarchy orig-hierarchy (set (keys orig-hierarchy)))))
 
 (defn- table-name [entity]
   (let [from (:from entity)]
@@ -55,7 +96,15 @@
     (cond-> (assoc first-entity :select (map #(field->select mapping %) select))
             (not-empty joins) (assoc :join joins))))
 
-(defn create-hierarchy [mapping]
-  (let [{:keys [entities joins]} mapping]
-    (->> joins
-         (reduce (fn [[e1 e2]])))))
+(defn parser-for [mapping]
+  (partial parse-query mapping))
+
+
+;   (->> joins
+;        (reduce (fn [[e1 e2]]))))
+#_
+(def mapping {:entities {:person {:from [:people]}
+                         :pet {:from [:pets]}
+                         :record {:from [:medicalrecord]}}
+              :joins {[:person :pet] [:= :pets.people_id :people.id]
+                      [:pet :record] [:= :pets.id :medicalrecord.pet_id]}})
--- test/bee_record/walker_test.clj
+++ test/bee_record/walker_test.clj
@@ -8,19 +8,49 @@
               :joins {[:person :pet] [:= :pets.people_id :people.id]
                       [:pet :record] [:= :pets.id :medicalrecord.pet_id]}})
 
+(def parse (walker/parser-for mapping))
+
+(facts "when generating the 'join map' (impl. detail)"
+  (fact "generates a 'direct' graph"
+    (#'walker/create-hierarchy {:entities {:person {:from [:people]} :pet {:from [:pets]}}
+                                :joins {[:person :pet] [:= :people.id :pets.person_id]}})
+    => {:person {:pet [:pets [:= :people.id :pets.person_id]]}
+        :pet {:person [:people [:= :people.id :pets.person_id]]}})
+
+  (fact "generates intermediate indexes"
+    (#'walker/create-hierarchy {:entities {:person {:from [:people]}
+                                           :pet {:from [:pets]}
+                                           :diag {:from [:diags]}
+                                           :medic {:from [:medics]}}
+                                :joins {[:person :pet] [:= :people.id :pets.person_id]
+                                        [:pet :diag] [:= :pets.id :diags.pet_id]
+                                        [:diag :medic] [:= :diag.medic_id :medics.id]}})
+    => {:person {:pet [:pets [:= :people.id :pets.person_id]]
+                 :medic :diag
+                 :diag :pet}
+        :pet {:person [:people [:= :people.id :pets.person_id]]
+              :diag [:diags [:= :pets.id :diags.pet_id]]
+              :medic :diag}
+        :diag {:medic [:medics [:= :diag.medic_id :medics.id]]
+               :pet [:pets [:= :pets.id :diags.pet_id]]
+               :person :pet}
+        :medic {:diag [:diags [:= :diag.medic_id :medics.id]]
+                :pet :diag
+                :person :pet}}))
+
 (fact "will generate a honey query with from"
-  (walker/parse-query mapping {:select [:person/id :person/name]})
+  (parse {:select [:person/id :person/name]})
   => {:select [[:people.id :person/id] [:people.name :person/name]]
       :from [:people]})
 
 (facts "when generating a query with a direct join"
-  (walker/parse-query mapping {:select [:person/name :pet/color :pet/race]})
+  (parse {:select [:person/name :pet/color :pet/race]})
   => {:select [[:people.name :person/name] [:pets.color :pet/color] [:pets.race :pet/race]]
       :from [:people]
       :join [:pets [:= :pets.people_id :people.id]]})
 
 (facts "when generating a query with indirect joins"
-  (walker/parse-query mapping {:select [:person/name :record/sickness]})
+  (parse {:select [:person/name :record/sickness]})
   => {:select [[:people.name :person/name] [:medicalrecord.sickness :record/sickness]]
       :from [:people]
       :join [:pets [:= :pets.people_id :people.id]
